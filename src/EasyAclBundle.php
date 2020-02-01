fe53d0ce5bdb5b37a09025d0fce87c686e993402
programarivm <info@programarivm.com>
2020-02-01 10:46:37 +0000

    first commit

---
+++ src/EasyAclBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Programarivm\EasyAclBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EasyAclBundle extends Bundle
+{
+    // TODO:
+    public function foo()
+    {
+        return 'foo';
+    }
+}
