5dcbbd96e7d5f705dfba61502265aaf304a9f618
programarivm <info@programarivm.com>
2020-02-15 10:44:24 +0000

    allowing entire services to be overridden

--- src/DependencyInjection/ProgramarivmEasyAclExtension.php
+++ src/DependencyInjection/ProgramarivmEasyAclExtension.php
@@ -5,6 +5,7 @@ namespace Programarivm\EasyAclBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ProgramarivmEasyAclExtension extends Extension
@@ -17,7 +18,10 @@ class ProgramarivmEasyAclExtension extends Extension
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
 
-        $definition = $container->getDefinition('programarivm_easy_acl.easy_acl');
+        $definition = $container->getDefinition('programarivm_easy_acl.lorem_ipsum');
+        if (null !== $config['word_provider']) {
+            $definition->setArgument(0, new Reference($config['word_provider']));
+        }
         $definition->setArgument(0, $config['unicorns_are_real']);
         $definition->setArgument(1, $config['min_sunshine']);
     }
