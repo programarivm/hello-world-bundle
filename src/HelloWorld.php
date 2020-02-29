ee41f8ca5ea975d05c814eb7a92a4f3a42a85bd4
programarivm <info@programarivm.com>
2020-02-29 17:37:48 +0000

    providers array added to the constructor in HelloWorld.php

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,14 +4,17 @@ namespace Programarivm\HelloWorldBundle;
 
 class HelloWorld
 {
+    private $providers;
+
     private $name;
 
     private $isExoplanet;
 
     private $satellites;
 
-    public function __construct(string $name = 'programarivm', bool $isExoplanet = true, int $satellites = 3)
+    public function __construct(array $providers, string $name = 'programarivm', bool $isExoplanet = true, int $satellites = 3)
     {
+        $this->providers = $providers;
         $this->name = $name;
         $this->isExoplanet = $isExoplanet;
         $this->satellites = $satellites;
